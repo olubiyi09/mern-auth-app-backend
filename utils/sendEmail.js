f73bf002bafb60c30b9b22aca82bbf157165f87f
olubiyi09 <olubiyi09@gmail.com>
2023-11-02 07:07:51 +0400

    make changes in my send email file

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-const hbs = require("nodemailer-express-handlebars");
+const expressHandlebars = require('express-handlebars');
 const path = require("path")
 
 const sendEmail = async (subject, send_to, sent_from, reply_to, template, name, link) => {
@@ -39,7 +39,7 @@ const sendEmail = async (subject, send_to, sent_from, reply_to, template, name, 
         extName: ".handlebars",
     }
 
-    transporter.use("compile", hbs(handlebarOptions))
+    transporter.use("compile", expressHandlebars(handlebarOptions))
 
     // Send Email
     transporter.sendMail(options, function (err, info) {
